1ceac4e623fd13e6f9845f6d51e025397953fbae
Su Specie <sspecie13@gmail.com>
2019-07-28 18:24:42 -0300

    Setting the routes

--- car-customization-simulator/src/app/app-routing.module.ts
+++ car-customization-simulator/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+import { EngineComponent } from './engine/engine.component';
+import { ColorComponent } from './color/color.component';
+import { WheelsComponent } from './wheels/wheels.component';
+import { SummaryComponent } from './summary/summary.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'engine', component: EngineComponent },
+  { path: 'color', component: ColorComponent },
+  { path: 'wheels', component: WheelsComponent},
+  { path: 'summary', component: SummaryComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
