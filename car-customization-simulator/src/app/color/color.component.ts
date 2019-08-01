6fe388809f00b409696a4353d1f86a260b8f0a0a
Su Specie <sspecie13@gmail.com>
2019-07-31 21:58:27 -0300

    Changing information according to color

--- car-customization-simulator/src/app/color/color.component.ts
+++ car-customization-simulator/src/app/color/color.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CustomizationService } from '../service/customization.service';
+import { ItemsColor } from '../models/items-color';
 
 @Component({
   selector: 'app-color',
@@ -11,14 +12,24 @@ export class ColorComponent implements OnInit {
   public wheelsLink = '/wheels';
   public colors = [];
   public description;
+  public urlCarImage: string;
+  public colorName: string;
+  public colorPrice: number;
 
 
   constructor(
     private service: CustomizationService,
   ) { }
 
-  ngOnInit() { 
+  ngOnInit() {
     this.callListColors();
+  }
+
+  public changeColor(color: ItemsColor): void {
+    this.urlCarImage = `../../assets/images/colors/${color.id}.png`;
+    this.colorName = color.label;
+    this.colorPrice = color.price;
+    console.log('mudando a cor');
   }
 
   private callListColors(): void {
@@ -29,6 +40,9 @@ export class ColorComponent implements OnInit {
           if (resp && resp.color && resp.color.items && resp.color.items.length > 0) {
             this.colors = resp.color.items;
             this.description = resp.color.description;
+            this.urlCarImage = `../../assets/images/colors/${resp.color.items[0].id}.png`;
+            this.colorName = resp.color.items[0].label;
+            this.colorPrice = resp.color.items[0].price;
           }
         }
       );
