bc0b6e6b8a7a3351f7347c81b1cc98501e4c7bd0
Su Specie <sspecie13@gmail.com>
2019-08-01 21:38:16 -0300

    Listing wheels

--- car-customization-simulator/src/app/wheels/wheels.component.ts
+++ car-customization-simulator/src/app/wheels/wheels.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomizationService } from '../service/customization.service';
 
 @Component({
   selector: 'app-wheels',
@@ -8,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
 export class WheelsComponent implements OnInit {
 
   public summaryLink = '/summary';
-  
-  constructor() { }
+  public wheels = [];
+
+  constructor(
+    private service: CustomizationService,
+  ) { }
 
   ngOnInit() {
+    this.callListWheels();
+  }
+
+  private callListWheels(): void {
+    this.service.list()
+      .subscribe(
+        (resp) => {
+          if (resp && resp.wheels && resp.wheels.items && resp.wheels.items.length > 0) {
+            this.wheels = resp.wheels.items;
+          }
+        }
+      );
   }
 
 }
